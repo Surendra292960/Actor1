b4210d462fa1572d71cab6c83d8cbb2070419bfc
Surendra292960 <surendrapalatm@outlook.com>
2018-11-15 09:24:23 +0530

    first commit

---
+++ ActorClient1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using HelloWorld.Interfaces;
+
+namespace ActorClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IHelloWorld actor = ActorProxy.Create<IHelloWorld>(ActorId.CreateRandom(), new Uri("fabric:/MyApplication/HelloWorldActorService"));
+            Task<string> retval = actor.GetHelloWorldAsync();
+            Console.Write(retval.Result);
+            Console.WriteLine("Please Enter Your name");
+            string name = Console.ReadLine();
+            Console.ReadLine();
+
+
+        }
+    }
+}
